cea187dac9e45be77b6f2c34dd2d7d286224cf79
maticamisay <maticamisay99@hotmail.com>
2022-07-01 00:18:09 -0300

    navbar terminado

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid ">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Frontend
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Html
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Css
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Javascript
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    React Js
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Angular
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Backend
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Node
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Express
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Mysql
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Diseño Ui
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Sobre mi
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline btn-light" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
