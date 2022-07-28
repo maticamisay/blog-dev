02382fb2afd21a22816e736fb9b6ee3dbdbab8c3
maticamisay <maticamisay99@hotmail.com>
2022-07-28 01:11:00 -0300

    posts services con axios

--- src/App.js
+++ src/App.js
@@ -8,35 +8,27 @@ import ContainerSuscribirse from './containers/ContainerSuscribirse';
 import ContainerBuscador from './containers/ContainerBuscador';
 import Footer from './components/Footer';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
-
+import postService from './services/posts';
 
 
 function App() {
   const [data, setData] = useState('')
-  const getFecht = async () => {
-    try {
-      const resp = await fetch('http://localhost:3001/api/posts')
-      // const resp = await fetch('https://blog-dev-back.herokuapp.com/')
-      const dataParse = await resp.json()
-      setData(dataParse)
-    } catch (error) {
-      console.log(error)
-    }
-
-  }
-
   useEffect(() => {
-    getFecht()
+    postService
+      .getAll()
+      .then(initialPosts => {
+        setData(initialPosts)
+      })
   }, [])
 
   return (
     <>
+    {/* https://medium.com/how-to-react/use-react-router-link-with-bootstrap-315a8b88e129 */}
       <Navbar />
       <Hero />
       <Categories />
       <ContainerPost title={'Ultimos Posts'} titleBtn={'Ver más'} data={data} />
-      <ContainerPost title={'Posts más leídos'} titleBtn={'Ver más'} data={data}/>
+      <ContainerPost title={'Posts más leídos'} titleBtn={'Ver más'} data={data} />
       <ContainerTag title={'Busca por nuestros tags'} />
       <ContainerSuscribirse title={'¡Suscribete a nuestro canal de noticias!'}
         description={'Para no perderte nada del mejor blog sobre programación, diseño y más.'}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,60 @@
 import React from "react";
-import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 const NavBar = () => {
   return (
     <>
-      <Navbar collapseOnSelect bg="dark" variant="dark" expand='md'>
+      <Navbar collapseOnSelect bg="dark" variant="dark" expand="md">
         <Container>
           <Navbar.Brand href="#home">Blog</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="px-3">
-            <NavDropdown title="Frontend" id="basic-nav-dropdown" className="px-1">
-              <NavDropdown.Item href="#action/3.1">HTML</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">CSS</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Javascript</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">React Js</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Backend" id="basic-nav-dropdown" className="px-1">
-              <NavDropdown.Item href="#action/3.1">Node Js</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Express</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">MySql</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#diseño-ui" className="px-1">Diseño Ui</Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="primary">Search</Button>
-          </Form>
+            <Nav className="px-3">
+              <NavDropdown
+                title="Frontend"
+                id="basic-nav-dropdown"
+                className="px-1"
+              >
+                <NavDropdown.Item href="#action/3.1">HTML</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">CSS</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Javascript
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">React Js</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                title="Backend"
+                id="basic-nav-dropdown"
+                className="px-1"
+              >
+                <NavDropdown.Item href="#action/3.1">Node Js</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Express</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">MySql</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#diseño-ui" className="px-1">
+                Diseño Ui
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="primary">Search</Button>
+            </Form>
           </Navbar.Collapse>
+          <Nav.Link href="#diseño-ui" className="px-1">
+            Iniciar Sesion
+          </Nav.Link>
         </Container>
       </Navbar>
     </>
